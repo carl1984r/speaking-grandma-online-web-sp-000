e95dfa82e700fde0a46e0e965adac3fbf3dabb88
Bry Cox <carl.cc@gmail.com>
2019-01-26 01:32:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase.upcase != true && phrase != 'I LOVE YOU GRANDMA!'
       return "HUH?! SPEAK UP, SONNY!"
+      binding.pry
     else phrase.upcase == true || phrase != 'I LOVE YOU GRANDMA!'
       return 'NO, NOT SINCE 1938!'
 end
