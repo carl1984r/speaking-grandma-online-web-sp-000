46e4ba57863c95184bd067c19c1c85b78f17dc17
Bry Cox <carl.cc@gmail.com>
2019-01-26 01:50:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,8 @@
 
 # Write a speak_to_grandma method.
-def speak_to_grandma?(phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase.upcase
-      return 'NO, NOT SINCE 1938!'
-    else
-      return 'HUH?! SPEAK UP, SONNY!'
-end
+def speak_to_grandma(phrase)
+
+
 end
 
 
